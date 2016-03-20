d10b30d6f2d2db1e442361ae304575b6dada7037
dodikzaki <doddyzaki@gmail.com>
2016-03-20 11:32:11 +0700

    terakhir

--- app/src/main/java/com/example/anonym/justjava/MainActivity.java
+++ app/src/main/java/com/example/anonym/justjava/MainActivity.java
@@ -23,11 +23,36 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         displayQuantity(quantity);
-        String price = "$"+ (quantity * 5);
-        price = price + "\nThank You!";
-        displayPrice(price);
+        String orderSummary = createOrderSummary();
+        displayMessage(orderSummary);
         //Coba commit
     }
+
+    /**
+     * method for order summary
+     * @return order summary
+     */
+
+    public String createOrderSummary(){
+        int priceValue;
+        priceValue = calculatePrice();
+        String price = "Name: Doddy Zaki";
+        price = price + "\nQuantity: " + quantity;
+        price = price + "\nTotal: $" + priceValue;
+        price = price + "\nThank You!";
+        return price;
+    }
+
+    /**
+     *
+     * method for Calculate Price
+     * @return price
+     */
+    private int calculatePrice(){
+        int price = quantity * 5;
+        return price;
+    }
+
 
     /**
      * This method is called when the increment button is clicked.
@@ -48,19 +73,19 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void displayQuantity(int number) {
+    private void displayQuantity(int numberOfOrder) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numberOfOrder);
     }
 
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(String a) {
+    private void displayMessage(String a) {
 
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        //priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-        priceTextView.setText(a);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        //orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        orderSummaryTextView.setText(a);
     }
 }
